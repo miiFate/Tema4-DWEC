18320bdef35b882927b8fc3f7f47fc6a43c00512
Antonio Villegas Arcos <32360762+miiFate@users.noreply.github.com>
2017-10-27 12:47:23 +0200

    Add files via upload

---
+++ W2/Ejercicio 1/script.js
@@ -0,0 +1,13 @@
+function temaNormal(){
+    document.getElementById("enlace").style.backgroundColor = "#a3a3dd";
+    document.getElementById("enlace").style.border = "2px solid Blue"
+    document.getElementById("enlace").style.borderTop = "#a3a3dd";
+    document.getElementById("enlace").style.borderLeft = "#a3a3dd";
+    document.getElementById("enlace").style.text
+
+    
+}
+
+function temaMinimalista(){
+    
+}
